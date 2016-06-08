9be59ffac9887ff27c79ccf79dcdb11854e28a67
Yann Prono <yann.prono@telecomnancy.net>
2016-06-08 14:25:42 +0200

    Add class Media

---
+++ src/Media.java
@@ -0,0 +1,33 @@
+/**
+ * A media is an abstract class, representing any kind of media (image, video, sound, animated image ...).
+ * A Media is defined by its path on the hard drive (for the moment).
+ *
+ * @author Mcdostone
+ */
+public class Media {
+
+    private String path;
+
+    /**
+     * Constructor
+     * @param path Filepath to access to this media
+     */
+    public Media(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    /**
+     * @param o Object to compare
+     * @return {@code true} only if the two medias have the same filepath.
+     */
+    public boolean equals(Object o) {
+        if(!(o instanceof Media)) return false;
+        return this.getPath().equals(((Media) o).getPath());
+    }
+
+    public String toString() {  return "[M] "  + this.getPath();  }
+}
